817558f9e6ecd2727f6c9d9b9b297acb78e76999
Salawat9048 <salawat9048@outlook.com>
2024-06-09 18:51:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+grades =[[5, 3, 3.0, 5, 4,], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+print(grades)
+grades[0] =float((5+3+3+5+4)/5)
+print(grades[0])
+grades[1] =float((2+2+2.+3)/4)
+print(grades[1])
+grades[2] =float((4+5+5+2)/4)
+print(grades[2])
+grades[3] =float((4+4+3)/3)
+print(grades[3])
+grades[4] =float((5+5+5+4+5)/5)
+print(grades[4])
+students_ ={'Aaron', 'Bilbo', 'Johnny', 'Khendrik', 'Steve'}
+average_rating ={'Aaron': 4.0, 'Bilbo': 2.25, 'Johnny': 4.0, 'Khendrik': 3.6666666666666665, 'Steve': 4.8}
+print(average_rating)
